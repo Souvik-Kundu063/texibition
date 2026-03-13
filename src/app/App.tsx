5ab25ffa8c7bffeae3ce71dfd298d737fd73901d
Subhajit Pathak <subhajitpathak9900@gmail.com>
2026-03-13 21:48:32 +0530

    ADD TEMPORARY LANDING PAGE

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -19,6 +19,8 @@ const Team = lazy(() => import('./pages/Team').then(module => ({ default: module
 const EventGallery = lazy(() => import('./pages/EventGallery').then(module => ({ default: module.default })));
 const FAQ = lazy(() => import('./pages/FAQ').then(module => ({ default: module.FAQ })));
 
+const TempLanding = lazy(() => import('./pages/TempLanding').then(module => ({ default: module.default })));
+
 // Loading skeleton component
 function PageLoader() {
   return (
@@ -59,28 +61,43 @@ function MerchPopupManager() {
   );
 }
 
+function AppContent() {
+  const location = useLocation();
+
+  return (
+    <div className="min-h-screen bg-[#0a0a0f]">
+      {location.pathname !== '/' && (
+        <>
+          <NavigationFuturistic />
+          <MobileBackButton />
+        </>
+      )}
+      {/* MerchPopupManager disabled per request */}
+      {/* <MerchPopupManager /> */}
+      <Suspense fallback={<PageLoader />}>
+        <Routes>
+          <Route path="/" element={<TempLanding />} />
+          <Route path="/texibition-2k26" element={<Home />} />
+          <Route path="/events" element={<Events />} />
+          <Route path="/events/:id" element={<EventDetails />} />
+          <Route path="/schedule" element={<Schedule />} />
+          <Route path="/code-of-conduct" element={<CodeOfConduct />} />
+          <Route path="/sponsors" element={<Sponsors />} />
+          <Route path="/team" element={<Team />} />
+          <Route path="/event-gallery" element={<EventGallery />} />
+          <Route path="/faq" element={<FAQ />} />
+        </Routes>
+      </Suspense>
+      {location.pathname !== '/' && <Footer />}
+    </div>
+  );
+}
+
 export default function App() {
   return (
     <BrowserRouter>
       <ScrollToTop />
-      <div className="min-h-screen bg-[#0a0a0f]">
-        <NavigationFuturistic />
-        <MobileBackButton />
-        <Suspense fallback={<PageLoader />}>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/events" element={<Events />} />
-            <Route path="/events/:id" element={<EventDetails />} />
-            <Route path="/schedule" element={<Schedule />} />
-            <Route path="/code-of-conduct" element={<CodeOfConduct />} />
-            <Route path="/sponsors" element={<Sponsors />} />
-            <Route path="/team" element={<Team />} />
-            <Route path="/event-gallery" element={<EventGallery />} />
-            <Route path="/faq" element={<FAQ />} />
-          </Routes>
-        </Suspense>
-        <Footer />
-      </div>
+      <AppContent />
     </BrowserRouter>
   );
 }
